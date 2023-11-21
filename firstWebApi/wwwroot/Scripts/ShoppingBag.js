eea9ac48573ac41ded5b8a73277d49556acaa2db
3257725sc <sc3257725@gmail.com>
2023-11-21 15:10:28 +0200

    change the quantity colmun, and did function to delete product from cart

--- firstWebApi/wwwroot/Scripts/ShoppingBag.js
+++ firstWebApi/wwwroot/Scripts/ShoppingBag.js
@@ -10,20 +10,39 @@ const showBagItems = (bagItems) => {
     console.log(bagItems);
     const template = document.getElementById("temp-row").content;
     const tbody = document.querySelector("tbody");
-    const prodArray = bagItems;
+    const prodId = [];
     bagItems.forEach(item => {
-        const clone = template.cloneNode(true);
-        const image = clone.querySelector(".image");
-        const itemName = clone.querySelector(".itemName");
-        const itemNumber = clone.querySelector(".itemNumber");
-        const quantity = clone.querySelector(".quantityColumn");
+        if (prodId.includes(item.id)) {
+            const prod = document.getElementById(item.id);
+            const quantityDiv = prod.querySelector('.quantity').innerHTML++;
+            console.log(quantityDiv);
+        }
+        else {
+            const clone = template.cloneNode(true);
+            const image = clone.querySelector(".image");
+            const itemName = clone.querySelector(".itemName");
+            const itemNumber = clone.querySelector(".itemNumber");
+            clone.querySelector("tr").id = item.id;
+            prodId.push(item.id);
 
-        image.src = "./Images/" + item.prodImage;
-        itemName.innerText = item.prodName;
-        itemNumber.innerText += " "+item.price ;
-        tbody.appendChild(clone);
+            image.src = "./Images/" + item.prodImage;
+            itemName.innerText = item.prodName;
+            itemNumber.innerText += " " + item.price;
+            tbody.appendChild(clone);
+        } 
         sum += item.price;
     })
     document.getElementById("itemCount").innerHTML = bagItems.length;
     document.getElementById("totalAmount").innerHTML = sum ;
+}
+
+const deleteProd = () => {    
+    const deleteButton = event.target;
+    const row = deleteButton.closest('.item-row'); 
+    const rowId = row.id;
+    console.log('Clicked on delete button of row with ID:', rowId);
+    const itemsBag = JSON.parse(sessionStorage.getItem("productArray"));
+    const prod = itemsBag.filter(prod => prod.id !== parseInt(rowId));
+    sessionStorage.setItem("productArray", JSON.stringify(prod));
+    showBagItems(prod);    
 }
