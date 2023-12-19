fc1a378c08afefef02a8a11d34eb018d19218d73
3257725sc <sc3257725@gmail.com>
2023-12-19 23:23:59 +0200

    clean code

--- firstWebApi/middlewares/RatingMiddleware.cs
+++ firstWebApi/middlewares/RatingMiddleware.cs
@@ -1,45 +1,4 @@
-﻿//using Entities.Models;
-//using Services;
-
-//namespace firstWebApi.middlewares
-//{
-//    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
-//    public class RatingMiddleware
-//    {
-//        private readonly RequestDelegate _next;
-//        private IRatingService _ratingService;
-
-//        public RatingMiddleware(RequestDelegate next)
-//        {
-//            _next = next;
-//        }
-
-//        public async Task<Task> Invoke(HttpContext httpContext, IRatingService ratingService)
-//        {
-//            _ratingService = ratingService;
-//            Rating rating = new Rating()
-//            {               
-//                Host = httpContext.Request.Host.ToString(),
-//                Method = httpContext.Request.Method,
-//                Path = httpContext.Request.Path,
-//                Referer = httpContext.Request.Host + httpContext.Request.Protocol,
-//                UserAgent = httpContext.Request.Headers["User-Agent"],
-//                RecordDate = new DateTime()
-//            };            
-//            _ratingService.AddRating(rating);
-//            return _next(httpContext);
-//        }
-//    }
-
-//    // Extension method used to add the middleware to the HTTP request pipeline.
-//    public static class RatingMiddlewareExtensions
-//    {
-//        public static IApplicationBuilder UseRatingMiddleware(this IApplicationBuilder builder)
-//        {
-//            return builder.UseMiddleware<RatingMiddleware>();
-//        }
-//    }
-//}
+﻿
 
 using Entities.Models;
 using Services;
